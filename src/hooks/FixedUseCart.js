2f8f723bf6000b0363151479dbea4085c68cdd9c
fathy <2021170827@cis.asu.edu.eg>
2025-08-01 17:41:55 +0300

    .

--- src/hooks/FixedUseCart.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import CartContextModule from "../context/CartContext"; 
-const useCart = () => {
-  return useContext(CartContextModule.CartContext); 
-};
-
-export { useCart };
